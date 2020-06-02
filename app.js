25412c36dc7105f010d9ee373b291e9d8d68e7b4
Subin Siby <mail@subinsb.com>
2020-06-02 15:29:46 +0530

    bind to port if mentioned in env

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.use(function (err, req, res, next) {
 
 db.connect()
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
